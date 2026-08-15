de23a45c10da6d7f20287da9b64431aabea85d48
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-08-15 20:37:56 +0530

    api doc added

--- 1-mongodb.sh
+++ 1-mongodb.sh
@@ -34,5 +34,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOGS_FILE
-VALIDATE $? "Installing MongoDB"
+ VALIDATE $? "Installing MongoDB"
 
