a06bb0d13eb0aeffc955664a8324eaf07cbb3d75
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-08-15 20:05:30 +0530

    api doc added

--- 1-mongodb.sh
+++ 1-mongodb.sh
@@ -4,27 +4,31 @@ LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
-LOG_FILE="$LOGS_FOLDER/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
 if [ $USERID -ne 0 ]; then
-    echo  "$TIMESTAMP [ERROR] Please run this script with root access"  | tee -a $LOG_FILE
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] $2....FAILURE" | tee -a $LOG_FILE
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$TIMESTAMP [INFO] $2....SUCCESS" | tee -a $LOG_FILE
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
-
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y &>> $LOG_FILE
+dnf install mongodb-org -y &>> $LOGS_FILE
 VALIDATE $? "Installing MongoDB"
